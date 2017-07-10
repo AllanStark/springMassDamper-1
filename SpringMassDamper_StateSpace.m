2ff81ce402894fa32ac59e9565322837b960dcd3
Michael Klamkin <klamike@users.noreply.github.com>
2017-07-10 11:42:53 -0400

    Update SpringMassDamper_StateSpace.m

--- SpringMassDamper_StateSpace.m
+++ SpringMassDamper_StateSpace.m
@@ -9,9 +9,9 @@ tic % for timing
 
 k = 24;% spring constant
 
-b = 8;% damping coefficient
+c = 8;% damping coefficient % use formula c^2 - 4*m*k = 0 to find critical damping
 
-m = 250;% mass in grams
+m = 200;% mass in grams
 
 stpAmp = m/1000 * 9.81; % step amplitude for step() function
 
@@ -37,7 +37,7 @@ display(toc, 'Time Variables'); % for timing
 tic % for timing
 
 A = [0 ,   1; 
-   -k/m, -b/m]; % for statespace
+   -k/m, -c/m]; % for statespace
 
 B = [0;
     1/m]; % for statespace
@@ -57,28 +57,29 @@ u = stpAmp * ones(1, nSmp); % constant force of gravity
 x_plot = zeros(2, nSmp); % preallocation for speed
 
 display(toc, 'Preallocation'); % for timing
-%% Automated Matlab Step Response Plotting (Continous Time)
+%% Continous - Automated Matlab Step Response + Plotting
 tic % for timing
 
 sysC = ss(A, B, C, D); % establishes continuous time statespace system
 
 figure(1); % new figure
+    subplot(2, 2, 1);
     step(sysC, setAmp); % graph step response
     title('Continous Time - Automated'); % change title
     xlim([0, duration]) % sets xlimit to duration
     
-display(toc, 'Automated Matlab Step Response + Plotting (continous)'); % for timing
-%% Continous Time to Discrete Time Conversion and Plotting
+display(toc, 'Continous - Automated Matlab Step Response + Plotting'); % for timing
+%% Discrete - Automated Matlab Step Response + Plotting
 tic % for timing
 
 sysD = c2d(sysC, Ts); % convert sysC(continous) to sysD (discrete)
 
-figure(2); % new figure
+subplot(2, 2, 2); % new figure
     step(sysD, setAmp); % automated matlab  step response for discrete time conversion (to compare to for loop)
     title('Discrete Time - Automated');
     xlim([0, duration]) % sets xlimit to duration
     
-display(toc, 'Continous to Discrete Time Conversion + Plotting '); % for timing
+display(toc, 'Discrete - Automated Matlab Step Response + Plotting'); % for timing
 %% Manual Spring-Mass-Damper System Calculation
 tic % for timing
 
@@ -93,15 +94,48 @@ for i = 1:nSmp % loops nSmp times, setting i to current cycle #
 end % terminate for loop
 
 display(toc, 'Manual Spring-Mass-Damper System Calculation'); % for timing
-%% Plotting
+%% Plotting Manual Spring-Mass-Damper System 
 tic % for timing
 
-figure(3); % new figure
+subplot(2, 2, 3); % new figure
     fig3 = plot(t, x_plot(1,:), 'b', t, x_plot(2, :), 'r'); % plots position in blue and velocity in red (y axis) with time on x axis
-    leg = legend('Position', 'Velocity'); set(leg, 'FontSize', 15); % legend - posiiton is blue and velocity is red
-    title('Discrete Time - Manual'); % figure title
-    set(fig3, 'LineWidth', 1.5); set(gca, 'FontName', 'Times New Roman'); set(gca, 'TitleFontSizeMultiplier', 1.75); set(gca, 'FontSize', 13); set(gca, 'LabelFontSizeMultiplier', 1.5); % figure styling
+    leg = legend('Displacement', 'Velocity'); set(leg, 'FontSize', 10.125); % legend - posiiton is blue and velocity is red
+    title('Manual For Loop - Auto Discretization'); % figure title
+    set(fig3, 'LineWidth', 1.5); set(gca, 'FontName', 'Times New Roman'); set(gca, 'TitleFontSizeMultiplier', 1.25); set(gca, 'FontSize', 10); set(gca, 'LabelFontSizeMultiplier', 1.125); % figure styling
     ylabel('Displacement (m) and Velocity (m/s)'); % y axis label
     xlabel('Time (s)'); % x axis label
-display(toc, 'Plotting - Final'); % for timing
+    
+display(toc, 'Plotting Manual Spring-Mass-Damper System'); % for timing
+%% Manual Discretization of State Space
+
+G = expm((A * Ts)); % discretized A
+H = (A')*(G - eye(2))*(B); % discretized B
+J = C; % discretized C
+K = D; % discretized D
+
+x_dis = [0;0];
+y_dis = zeros(1, nSmp); % preallocation for speed
+x_dis_plot = zeros(2, nSmp);
+
+for i = 1:nSmp % loops nSmp times, setting i to current cycle #
+    
+    y_dis(i) = J * x_dis + K * u(i); % output - [position]
+    
+    x_dis = G * x_dis + H * u(i); % states - [position;velocity]
+
+    x_dis_plot(:,i) = x_dis; % for plotting X later
+    
+end % terminate for loop
+%% Plotting Manual Discretization of State Space
+
+subplot(2, 2, 4);
+    fig4 = plot(t, x_dis_plot(1,:), 'b', t, x_dis_plot(2, :), 'r'); % plots position in blue and velocity in red (y axis) with time on x axis
+    leg = legend('Displacement', 'Velocity'); set(leg, 'FontSize', 10.125); % legend - posiiton is blue and velocity is red
+    title('Manual For Loop - Manual Discretization'); % figure title
+    set(fig4, 'LineWidth', 1.5); set(gca, 'FontName', 'Times New Roman'); set(gca, 'TitleFontSizeMultiplier', 1.25); set(gca, 'FontSize', 10); set(gca, 'LabelFontSizeMultiplier', 1.125); % figure styling
+    ylabel('Displacement (m) and Velocity (m/s)'); % y axis label
+    xlabel('Time (s)'); % x axis label
+    
+display(toc, 'Plotting Manual Spring-Mass-Damper System'); % for timing
+
 display(toc(totalTime), 'Total Time'); % final timer
